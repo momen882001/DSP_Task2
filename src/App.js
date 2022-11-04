30ebfc049bf7d1a65494423b7a9d14061fc43498
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2022-11-04 18:05:17 +0200

    react-app

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import axios from "axios";
+import './App.css';
+
+function App() {
+
+  const [slider,setSlider] = useState(30);
+  let formData = new FormData();
+
+  const onfileChange = (e) => {
+     console.log(e.target.files[0] , "taaaaaaaaaaaaaaaaaaaamer")
+     if(e.target && e.target.files[0]) {
+      formData.append("file" , e.target.files[0])
+     }
+     axios.post('https://v2.convertapi.com/upload',{
+      formData
+    }).then((response) => {
+     console.log(response)
+    }).catch((err) => {
+     console.log(err)
+    })
+  } 
+
+  return (
+    <div className="App">
+
+      
+        <div>
+          <label>Select file</label>
+          <input type="file" name="file" onChange={onfileChange}/>
+        </div>      
+
+      <div class="slider">
+        <input type="range" min={0} max={200} value={slider} step={1} onChange={(e) => setSlider(e.target.value)} />
+        <p>{slider}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
