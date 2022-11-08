cb5ebd28df149f97612bf2ecc2a76c373fe390d3
AhmedHassan178 <87389435+AhmedHassan178@users.noreply.github.com>
2022-11-08 23:55:38 +0200

    make modified file of sound and pass it to route

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ from flask_cors import CORS
 from librosa import load
 import os
 from werkzeug.utils import secure_filename
+from logic import logic
+import numpy as np
+import soundfile
+from IPython.display import Audio
 app = Flask(__name__)
 CORS(app)
+UPLOADS_FOLDER = './DSP_Task2/static'
+app.config['UPLOADS_FOLDER'] = UPLOADS_FOLDER
 @app.route("/")
 def default():
     pass
@@ -14,11 +20,17 @@ def upload():
         if "file" not in request.files:
             return {"there is an error":'err'},400
         file = request.files["file"]
-        completeName = os.path.join('/static',file.filename)
-        file.save(completeName)
+        signal,sr = load(file)
+        f_signal,freqs = logic.fourier(signal,sr)
+        list_of_f = logic.select_range(freqs,0,15000,True)
+        signal_rec = np.fft.irfft(logic.modify_magnitude(list_of_f,f_signal,0.0))
+        completeName = os.path.join(app.config['UPLOADS_FOLDER'],'modified.mp3')
+        soundfile.write(completeName,signal_rec,sr)
         return("hello")
-    else:
-        return send_from_directory(app.config['static'],filename=file.filename)
+@app.route('/static/<path:filename>')
+def send_attachment(filename):
+  return send_from_directory(app.config['UPLOADS_FOLDER'], 
+    filename=filename)
 if __name__ == "__main__":
     app.run(debug=True,port='8080',host='0.0.0.0')
     
